734743ce489052e37e1db3974630332e0b3a9b20
zenonass <87283629+zenonass@users.noreply.github.com>
2022-11-04 11:32:59 +0200

    Update useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,7 +4,6 @@ const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
 
-    // using data/db.json as database with "npx json-server --watch data/db.json --port 8000"
     useEffect(() => {
         fetch(url)
             .then(res => {
